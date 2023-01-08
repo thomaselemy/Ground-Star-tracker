03cd215f49cd8f9eb76c1754cd76e2308b5722bb
thomaselemy <33559754+thomaselemy@users.noreply.github.com>
2023-01-08 14:21:40 -0800

    Add files via upload

--- main - Copy.py
+++ main - Copy.py
@@ -21,22 +21,26 @@ SerialObj.stopbits = 1     # Number of Stop bits = 1'''
 
 
 Px,Ix,Dx=-1/320,0,0
+
 Py,Iy,Dy=-0.2/240,0,0
+
 integral_x,integral_y=0,0
+
 differential_x,differential_y=0,0
+
 prev_x,prev_y=0,0
+
 pixelmovement = 10
-# Argument input
-#parser= argparse.ArgumentParser() #creating a parser
-#parser.add_argument("-v", "--video",required=True, help="path to video")
-#parser.add_argument("-t", "--tracker", type=str,
-#default="csrt", help="OpenCV tracker type")
-#parser.add_argument(, required=True, help="path to output")
-#args = vars(parser.parse_args()) #parsing arguments
 
 # Process argument
 print("[INFO] Start to process video")
 vs = cv2.VideoCapture(0)  # Initialize the video
+
+#vs.set(cv2.CAP_PROP_MONOCHROME)
+# set exposure time
+#time.sleep(2)
+
+trackrType = "csrt"
 writer = None
 label = " "
 fps = None
@@ -49,7 +53,7 @@ OPENCV_OBJECT_TRACKERS = {
     "medianflow": cv2.legacy.TrackerMedianFlow_create,
     "mosse": cv2.legacy.TrackerMOSSE_create
 }
-tracker = OPENCV_OBJECT_TRACKERS["medianflow"]()  # Initialize the tracker
+tracker = OPENCV_OBJECT_TRACKERS[trackrType]()  # Initialize the tracker
 
 box = None
 position = deque()
@@ -63,6 +67,7 @@ video_frame_rate = vs.get(cv2.CAP_PROP_FPS)  # Obtain video frame rate
 #create_GUI()
 while True:
     (grabbed, frame) = vs.read()  # Loop frame by frame
+
     if frame is None:  # End if there is no frame left
         break
     # frame=frame[100:900,800:1900] Reshape frame if necessary
@@ -90,37 +95,31 @@ while True:
             nMovementy = error_y / pixelmovement
             #print('pixelerrorx=',error_x,'valx=',valx)
             #print('pixelerrory=',error_y, 'valy=', valy)
-            '''if error_x > 5 and error_x < 20:  # if star x pos greater than +5 and less than 20, move camera slowly right
+            if error_x > 1:  # if star x pos greater than +5 and less than 20, move camera slowly right
                 # text#SerialObj.write(b'R')
-                print('print small move right')
+                #print('print small move right')
                 horz_motorCommand = b'l'
-            elif error_x >= 20:
-                horz_motorCommand = b'L'
-                print("big right move")
-
-            elif error_x < -5 and error_x > -20:
+            elif error_x < -1:
                 print("small move left")
                 horz_motorCommand = b'r'
-            elif error_x <= -20:
-                print("big left move")
-                horz_motorCommand = b'R'
             else:
                 horz_motorCommand = None  # dont need to move camera
 
                 # check if horizontal correction necessary
             if horz_motorCommand:
-                # check if motor command is small movment
+                # check if motor command is small movement
                 if horz_motorCommand.isupper():
-                    deltaX = 4  # THOMAS NEED TO DETERMINE THIS NUMBER
+                    deltaX = 5  # THOMAS NEED TO DETERMINE THIS NUMBER
                     move_duration = 0.1  # seconds. THOMAS NEED TO DETERMINE THIS
                 else:
-                    deltaX = 20  # THOMAS NEED TO DETERMINE THIS NUMBER BIG MOVEMENTS
+                    deltaX = 60  # THOMAS NEED TO DETERMINE THIS NUMBER BIG MOVEMENTS
                     move_duration = 0.2  # seconds. THOMAS NEED TO DETERMINE THIS
                 if tstart:
                     elapsed_duration = time.time() - tstart
                 # compute number of commands that need to be sent to motor
                 nHorizontal_moves = round(abs(error_x) / deltaX)
-                if elapsed_duration > 2: #nHorizontal_moves * move_duration:
+                print(f"N moves: {nHorizontal_moves}")
+                if elapsed_duration > 5: #nHorizontal_moves * move_duration:
                     tstart = None
                 if not tstart:
 
@@ -128,23 +127,17 @@ while True:
                     # send movement commands to motor
                     for _ in range(nHorizontal_moves):
                             SerialObj.write(horz_motorCommand)
-                            print("move right/left")'''
+                            #print("move right/left")
 
             #  yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy movement
-            if error_y > 5 and error_y < 20:  # if star y pos greater than +5 and less than 20, move camera down
+
+            if error_y > 1:  # if star y pos greater than +5 and less than 20, move camera down
                 # text#SerialObj.write(b'R')
                 print('print small down')
-                vert_motorCommand = b'd'
-            elif error_x >= 20:
-                vert_motorCommand = b'D'
-                print("big down move")
-
-            elif error_y < -5 and error_y > -20:# if star y pos greater than +5 and less than 20, move camera down
-                print("small move up")
                 vert_motorCommand = b'u'
-            elif error_y <= -20:
-                print("big down move")
-                vert_motorCommand = b'U'
+            elif error_y < -1:# if star y pos greater than +5 and less than 20, move camera down
+                print("small move up")
+                vert_motorCommand = b'd'
             else:
                 vert_motorCommand = None  # dont need to move camera
 
@@ -152,32 +145,34 @@ while True:
             if vert_motorCommand:
                 # check if motor command is small movment
                 if vert_motorCommand.isupper():
-                    deltaY = 6  # THOMAS NEED TO DETERMINE THIS NUMBER
+                    deltaY = 50  # THOMAS NEED TO DETERMINE THIS NUMBER
                     move_duration2 = 0.1  # seconds. THOMAS NEED TO DETERMINE THIS
                 else:
-                    deltaY = 8  # THOMAS NEED TO DETERMINE THIS NUMBER BIG MOVEMENTS
+                    deltaY = 60  # THOMAS NEED TO DETERMINE THIS NUMBER BIG MOVEMENTS
                     move_duration2 = 0.2  # seconds. THOMAS NEED TO DETERMINE THIS
                 if tstart2:
                     elapsed_duration2 = time.time() - tstart2
                 # compute number of commands that need to be sent to motor
                 nVertical_moves = round(abs(error_y) / deltaY)
-                if elapsed_duration2 > 10: #nVertical_moves * move_duration2:
+                if elapsed_duration2 > 5: #nVertical_moves * move_duration2:
                     tstart2 = None
                 if not tstart2:
 
                     tstart2 = time.time()
                     # send movement commands to motor
                     for _ in range(nVertical_moves):
-                        SerialObj.write(vert_motorCommand)
+                        SerialObj.write(vert_motorCommand )
                         print("move up/down")
 
 
-
+            print(error_x, error_y)
+            print(center_x, center_y)
             # Draw a rectangle around the object
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
             position.appendleft((int(x + w / 2), int(y + h / 2)))
             for i in range(0, len(position)):
                 if i + 1 < len(position):
+                    #print()
                     # Draw the trajectory
                     cv2.line(frame, position[i], position[i + 1], (255, 0, 0), 2)
 
@@ -186,7 +181,7 @@ while True:
         fps.stop()
 
         # Print info in each frame
-        info = [("Tracker", "medianflow"), ("Success", "Yes" if success else "No"),
+        info = [("Tracker", trackrType), ("Success", "Yes" if success else "No"),
                 ("FPS:", "{:.2f}".format(fps.fps())), ("Position", position[0])]
         for (i, (k, v)) in enumerate(info):
             text = "{}:{}".format(k, v)
@@ -215,7 +210,7 @@ while True:
     elif key == ord("c"):
         box = None
         state = 0
-        tracker = OPENCV_OBJECT_TRACKERS["medianflow"]()
+        tracker = OPENCV_OBJECT_TRACKERS[trackrType]()
         position = deque()
 
     #writer.write(frame)
@@ -224,4 +219,4 @@ if writer:
     writer.release()
 vs.release()
 cv2.destroyAllWindows()
-#SerialObj.close()          # Close the port
+SerialObj.close()          # Close the port
